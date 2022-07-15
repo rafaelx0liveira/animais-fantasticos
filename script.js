72e3f3f927519e6554d37d747f6cc6f97af6e1d1
Rafael Oliveira <rafaelaparecido.oliveirasilva@gmail.com>
2022-07-15 12:09:52 -0300

    Navegação por Tabs

---
+++ script.js
@@ -0,0 +1,24 @@
+/* 
+  Relacionar cada imagem com a sua descrição. Para isso, vamos fazer um menu de navegação relacionando cada li com sua section de descrição
+*/ 
+
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabConteudo = document.querySelectorAll('.js-tabconteudo section');
+
+/* 
+  Ativar tab de acordo com o index. Para isso, adicionamos a classe 'ativo' na section de acordo com o index, e 
+  removemos essa classe das outras sections
+*/
+function activeTab(index){
+  tabConteudo.forEach((section) => {
+    section.classList.remove('ativo');
+  })
+  tabConteudo[index].classList.add('ativo');
+}
+
+/* Loop para cada tab li. De argumentos, temos o itemMenuu e o index, que se refere a cada posição da imagem. Para cada item é adicionado um evento de clique. E a partir desse evento, quando for clicado, vai ser acionada a função anônima para passar para a função activeTab o index da imagem  */
+tabMenu.forEach((itemMenu, index) =>{
+  itemMenu.addEventListener('click', () => {
+    activeTab(index);
+  });
+});
